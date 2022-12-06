808e27190f3c5382e19559f7930d17bf5aed8187
alakhpandya <alakhpandya@gmail.com>
2022-12-06 20:03:51 +0530

    dec6 commit

--- CSS/Project 1/js/main.js
+++ CSS/Project 1/js/main.js
@@ -1,7 +1,11 @@
 $('.owl-carousel').owlCarousel({
-    // loop:true,
-    margin:10,
+    loop:true,
+    // margin: 0,
     nav:true,
+    dots: false,
+    // autoplay: true,
+    // autoplayTimeout: 3000,
+    navText: ['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>'],
     // nav:false,
     responsive:{
         0:{
@@ -14,4 +18,4 @@ $('.owl-carousel').owlCarousel({
         //     items:5
         // }
     }
-})
+}) 
