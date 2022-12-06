e2fe8793f0376e050c2a9a2da699b9b5d5eff80f
alakhpandya <alakhpandya@gmail.com>
2022-12-06 18:13:36 +0530

    Initial commit

---
+++ CSS/Project 1/js/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    // loop:true,
+    margin:10,
+    nav:true,
+    // nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+        // 1000:{
+        //     items:5
+        // }
+    }
+})
