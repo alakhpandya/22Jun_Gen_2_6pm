d50f6f87e87b4823f253dcedc8bbf4aa3d118075
alakhpandya <alakhpandya@gmail.com>
2023-02-05 15:02:33 +0530

    feb5 commit

---
+++ javascript/jan31.js
@@ -0,0 +1,97 @@
+/*
+while, do-while, for
+*/
+
+// while loop
+/*
+let i = 0
+let arr = [];
+let n = Number(prompt("Number of elements:"))
+while (i < n){
+    element = prompt("Element:")
+    arr.push(element);
+    i++;
+}
+console.log(arr);
+*/
+
+/*
+do{
+    // code
+}while(condition);
+*/
+
+// Write a program that counts number of digits of a given number. 4199
+
+// console.log(Math.floor(14 / 5));
+// console.log((14 / 5));
+
+// c = 0
+// n = Number(prompt("n:"))
+/*
+console.log(Math.floor(n / 10));
+c++;
+n = Math.floor(n / 10);
+console.log(c);
+
+console.log(Math.floor(n / 10));
+c++;
+n = Math.floor(n / 10);
+console.log(c);
+
+console.log(Math.floor(n / 10));
+c++;
+n = Math.floor(n / 10);
+console.log(c);
+
+console.log(Math.floor(n / 10));
+c++;
+n = Math.floor(n / 10);
+console.log(c);
+*/
+/*
+do{
+        c++;
+        n = Math.floor(n / 10);
+}while(n > 0)
+
+console.log(c);
+*/
+
+/*
+array2d = [
+    [12, 34, 98],
+    ["javascript", "Python", "C", "C++"],
+    ["Royal", "Oracle"]
+]
+
+*/
+/*
+let array2d = [];
+var row;
+var row_no = Number(prompt("Number of rows:"));
+for(let i = 0; i < row_no; i++){
+    entries = Number(prompt("No of entries in this row:")); //3
+    row = [];
+    for(let j = 0; j < entries; j++){
+        data = prompt("Entry:");
+        row.push(data);
+    }
+    array2d.push(row);
+}
+console.log(array2d);
+*/
+/*
+array2d = [
+    [12, 34, 98, 20, 11],
+    ["javascript", "Python", "C"],
+    ["Royal", "Oracle", "java"]
+]
+*/
+
+/*
+Write the following codes:
+1. To add two 3x3 matrix
+2. To multiply two 3x3 matrix
+3. To find determinant of a 3x3 matrix
+*/
