4ccb1da6f00816b17020555861b955e386297e59
alakhpandya <alakhpandya@gmail.com>
2023-03-31 20:04:29 +0530

    mar30

--- javascript/export_multiple.js
+++ javascript/export_multiple.js
@@ -1,7 +1,11 @@
 const capitalizeString = (str) => {
-    return str.charAt([0]).toUpperCase();
+    return str.charAt([0]).toUpperCase() + str.slice(1, str.length);
 };
+
+const capitalizeLastCharacter = (str) => {
+    return str.slice(0, str.length-1) + str.charAt([str.length - 1]).toUpperCase();
+}
 
 export const pi = 3.1415;
 export const e = 2.73;
-export {capitalizeString}
+export {capitalizeString, capitalizeLastCharacter}
